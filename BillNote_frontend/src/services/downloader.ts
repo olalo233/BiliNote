0b7b2596fc115ceb2202ec84c862267cc2bce6ff
olalo <49304388+olalo233@users.noreply.github.com>
2026-07-13 15:37:08 +0800

    fix(cookie): add downloader cookie deletion

--- BillNote_frontend/src/services/downloader.ts
+++ BillNote_frontend/src/services/downloader.ts
@@ -7,3 +7,7 @@ export const getDownloaderCookie = async id => {
 export const updateDownloaderCookie = async (data: { cookie: string; platform: any }) => {
   return await request.post('/update_downloader_cookie', data)
 }
+
+export const deleteDownloaderCookie = async (platform: string) => {
+  return await request.delete('/delete_downloader_cookie/' + platform)
+}
